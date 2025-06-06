bec46a53e38d01733c00d3b112771b0be0e1b882
IBhatt <ipsit8979178483@gmail.com>
2025-06-06 10:33:18 +0530

    push all

---
+++ src/java_basic/hello_world.java
@@ -0,0 +1,8 @@
+package java_basic;
+
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+        System.out.println("Hello Git");
+    }
+}
