f6bb9dcbbe7e2fd1978262742cefd606100da62c
IBhatt <ipsit8979178483@gmail.com>
2025-06-11 10:35:32 +0530

    Final Keyword Sample

---
+++ src/java_basic/Final_Example.java
@@ -0,0 +1,12 @@
+package java_basic;
+
+import java.sql.SQLOutput;
+
+public class Final_Example {
+    public static void main(String[] args) {
+        int local = 0;
+        System.out.println(local);
+        final float PI = 3.14f;
+        System.out.println(PI);
+    }
+}
