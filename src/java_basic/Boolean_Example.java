c8e4b3706b81fc5749112e1710696eba03c404f7
IBhatt <ipsit8979178483@gmail.com>
2025-06-11 10:40:38 +0530

    Final Keyword Sample

---
+++ src/java_basic/Boolean_Example.java
@@ -0,0 +1,10 @@
+package java_basic;
+
+public class Boolean_Example {
+    public static void main(String[] args){
+        boolean is_true = true;
+        boolean is_false = false;
+
+        System.out.println(is_true + " " + is_false);
+    }
+}
