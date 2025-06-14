123bcae13e704f0c7a6043d21499b0ee201e4ba3
IBhatt <ipsit8979178483@gmail.com>
2025-06-14 10:24:35 +0530

    Table of Nine using printf

---
+++ src/java_basic/Printf_table9.java
@@ -0,0 +1,11 @@
+package java_basic;
+
+public class Printf_table9 {
+    public static void main(String[] args) {
+        int num = 9;
+        System.out.printf("%d * %d = %d",9,1,9).println();
+        System.out.printf("%d * %d = %d",9,2,18).println();
+        System.out.printf("%d * %d = %d",9,3,27).println();
+        System.out.printf("%d * %d = %d",9,4,36);
+    }
+}
