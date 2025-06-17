f6f71189771ccee46ecaff0176dc0944dc737c18
IBhatt <ipsit8979178483@gmail.com>
2025-06-17 19:21:29 +0530

    Non Primitive types interview questions

---
+++ src/java_basic/InterviewQuestion.java
@@ -0,0 +1,8 @@
+package java_basic;
+
+public class InterviewQuestion {
+        public static void main(String[] args) {
+            long l = 10l; // 8 Byte, 64 Bits
+            String s = "name"; //  8 Byte, 64 Bits
+    }
+}
