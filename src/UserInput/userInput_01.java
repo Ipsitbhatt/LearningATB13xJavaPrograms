1f45f872194e66da7fe357910195bd3f5ca9145c
IBhatt <ipsit8979178483@gmail.com>
2025-06-29 11:53:49 +0530

    User Input from scanner

---
+++ src/UserInput/userInput_01.java
@@ -0,0 +1,14 @@
+package UserInput;
+
+import java.util.Scanner;
+
+public class userInput_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter age");
+        int age = scanner.nextInt();
+
+
+
+    }
+}
